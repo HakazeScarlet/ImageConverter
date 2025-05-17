5fab44f189de71e89d660847e21fb938b1f8ddf7
Hakaze S <frost.scarlet.163@gmail.com>
2025-05-18 01:37:53 +0300

    Added small fixes

--- app.py
+++ app.py
@@ -10,13 +10,13 @@ def convert_image():
     file_storage = request.files.get('image')
     colorful_image = Image.open(io.BytesIO(file_storage.read()))
 
-    grey_image = colorful_image.convert('L')
+    gray_image = colorful_image.convert('L')
 
-    grey_image_bytes = io.BytesIO()
-    grey_image.save(grey_image_bytes, format=colorful_image.format)
-    grey_image_bytes.seek(0)
-    gray_image_name = 'grey_' + file_storage.filename
-    return send_file(grey_image_bytes, mimetype=file_storage.mimetype, as_attachment=True, download_name=gray_image_name)
+    gray_image_bytes = io.BytesIO()
+    gray_image.save(gray_image_bytes, format=colorful_image.format)
+    gray_image_bytes.seek(0)
+    gray_image_name = 'gray_' + file_storage.filename
+    return send_file(gray_image_bytes, mimetype=file_storage.mimetype, as_attachment=True, download_name=gray_image_name)
 
 if __name__ == '__main__':
     app.run()
