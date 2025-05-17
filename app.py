d717b9f92aa69ffa2f66afa021aa2023430c2b7d
Hakaze S <frost.scarlet.163@gmail.com>
2025-05-17 21:31:46 +0300

    Implemented image converting

--- app.py
+++ app.py
@@ -1,17 +1,20 @@
-from flask import Flask, request
+import io
 
+from flask import Flask, request, send_file
+from PIL import Image
 app = Flask(__name__)
 
-
 @app.post('/convert_image')
-def convert_image(string):
-    string = input()
-    string = string.upper()
-    return string
-    # image = request.files['image']
-    # new_image = image.convert("L")
-    # return new_image
+def convert_image():
+    file_storage = request.files.get('image')
+    colorful_image_bytes = Image.open(io.BytesIO(file_storage.read()))
 
+    grey_image = colorful_image_bytes.convert('L')
+
+    grey_image_bytes = io.BytesIO()
+    grey_image.save(grey_image_bytes, format='JPEG')
+    grey_image_bytes.seek(0)
+    return send_file(grey_image_bytes, mimetype='image/jpeg', as_attachment=True, download_name=file_storage.filename)
 
 if __name__ == '__main__':
     app.run()
